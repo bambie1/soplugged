8da7eeb36fd919860d97348f5f94828579dae87a
Benaiah Barango <bennieb96@gmail.com>
2021-06-04 21:58:34 -0400

    added loader for merch page

--- pages/merch.js
+++ pages/merch.js
@@ -3,17 +3,42 @@ import displayShopifyCollection from "../src/shopifyStore";
 import { Typography, makeStyles, Box } from "@material/mui-components";
 import SEO from "@components/SEO";
 import SubscribeForm from "@components/SubscribeForm";
+import dynamic from "next/dynamic";
 
-const useStyles = makeStyles((theme) => ({}));
+const DynamicProgress = dynamic(() =>
+  import("@material/mui-components").then((mod) => mod.CircularProgress)
+);
+const DynamicAlert = dynamic(() =>
+  import("@material/mui-lab").then((mod) => mod.Alert)
+);
+
+const useStyles = makeStyles((theme) => ({
+  loading: {
+    position: "absolute",
+    top: "0",
+    left: "0",
+    width: "100%",
+    zIndex: "1",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+}));
 
 const Merch = () => {
+  const classes = useStyles();
   return (
     <>
       <SEO
         description="Custom tees, sweatshirts and accessories to spread the word"
         title="SoPlugged Merch"
       />
-      <Box mt={3} className="page">
+      <Box
+        mt={3}
+        className="page"
+        style={{ display: "flex", flexDirection: "column" }}
+      >
         <Typography
           variant="h4"
           style={{ textAlign: "center", fontWeight: "700" }}
@@ -27,14 +52,28 @@ const Merch = () => {
         <hr
           style={{ width: "40%", maxWidth: "170px", marginBottom: "40px" }}
         ></hr>
-        <div id="collection-component-1622397974663">
+        <div
+          id="collection-component-1622397974663"
+          style={{ position: "relative", display: "flex", minHeight: "250px" }}
+        >
           {displayShopifyCollection(
             "collection-component-1622397974663",
             "267757355198"
           )}
+          <div id="behind" className={classes.loading}>
+            <DynamicProgress
+              style={{ marginBottom: "16px" }}
+              color="secondary"
+              aria-label="loading progress"
+            />
+            <DynamicAlert severity="info">
+              Loading the store. Hang tight!
+            </DynamicAlert>
+          </div>
         </div>
-
-        <SubscribeForm />
+        <div style={{ marginTop: "auto" }}>
+          <SubscribeForm />
+        </div>
       </Box>
     </>
   );
