d500baaf781f26544303e5ac2c0deb560872001e
Benaiah Barango <bennieb96@gmail.com>
2022-01-07 09:41:05 -0500

    re--try

--- cypress/integration/dashboard.spec.ts
+++ cypress/integration/dashboard.spec.ts
@@ -8,12 +8,23 @@ describe("Dashboard", () => {
   it("user is prompted to add business if none exist", () => {
     cy.visit("test/login");
 
+    cy.intercept(
+      {
+        method: "GET",
+        hostname: "sopluggd-staging.herokuapp.com",
+        url: "/business", // that have a URL that matches '/users/*'
+      },
+      [] // and force the response to be: []
+    ).as("getBusiness");
+
     cy.findByRole("textbox").type("tim@tam.com");
     cy.findByLabelText("Password", { selector: "input" }).type("Isight23!");
 
     cy.findByRole("button", { name: /Login/i }).click();
 
     cy.visit("/dashboard");
+
+    cy.wait("@getBusiness");
     cy.findByRole("link", { name: /Add/i });
   });
 });
