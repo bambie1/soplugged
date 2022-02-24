00d20a8fa2867858d5388b6be299a4f9f1b869a1
Benaiah Barango <bennieb96@gmail.com>
2022-02-24 12:15:52 -0500

    featured styles

--- components/FeaturedGuide/FeaturedGuide.tsx
+++ components/FeaturedGuide/FeaturedGuide.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 import styles from "./FeaturedGuide.module.scss";
+import { Button } from "@/styled/Button";
 
 interface Props {
   feature: any;
@@ -10,11 +13,24 @@ const FeaturedGuide: FC<Props> = ({ feature }) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.grid}>
-        <div />
-        <div className={styles.content}>
-          <p>FEATURED</p>
-          <h2>{feature.title}</h2>
-          <p>{feature.excerpt}</p>
+        <div className={styles.featuredImage}>
+          <Image
+            src="/images/business-woman.jpg"
+            alt=""
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <div>
+          <Link href={`/pro/guides/${feature.slug}`}>
+            <a className={styles.content}>
+              <p className={styles.tag}>FEATURED</p>
+              <h3>{feature.title}</h3>
+              <p>{feature.excerpt}</p>
+
+              <Button variant="text">Continue reading</Button>
+            </a>
+          </Link>
         </div>
       </div>
     </div>
