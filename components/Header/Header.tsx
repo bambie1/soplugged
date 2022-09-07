4cc53a04ea2cdd36a45859f4f8b66f336cf6d382
Benaiah Barango <benaiah.barango@fullscript.com>
2022-09-07 17:20:23 -0400

    more mobile nav updates

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,18 +2,19 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 import { ButtonLink } from "@/styled/ButtonLink";
 
-import styles from "../../styles/Header.module.scss";
 import { ArrowButton } from "@/styled/ArrowButton";
 import MobileHeader from "./MobileHeader";
+
+import styles from "../../styles/Header.module.scss";
 
 const Searchbar = dynamic(() => import("../algolia/Searchbar"));
 
 const mainNav = [
-  { id: 1, text: "Directory", link: "/search" },
+  { id: 1, text: "Find a business", link: "/search" },
   { id: 2, text: "PRO", link: "/pro" },
   { id: 3, text: "Blog", link: "/blog" },
 ];
@@ -23,8 +24,17 @@ interface Props {
   showBanner?: boolean;
 }
 
-const Header: FC<Props> = ({ hideSearch }) => {
+const Header: FC<Props> = () => {
   const router = useRouter();
+  const [isStyled, setIsStyled] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", () =>
+        setIsStyled(window.pageYOffset > 40)
+      );
+    }
+  }, []);
 
   const buildStyles = (href: string) => {
     if (router.asPath.startsWith(href))
@@ -36,9 +46,13 @@ const Header: FC<Props> = ({ hideSearch }) => {
   return (
     <>
       <nav
-        className={`sticky top-0 z-20 hidden w-full overflow-hidden border-b bg-white transition duration-100 md:flex`}
+        className={`sticky top-0 z-20 w-full overflow-hidden ${
+          isStyled && "border-b"
+        } bg-white py-3 transition duration-100 md:py-0`}
       >
-        <div className="mx-auto w-full max-w-7xl px-2 sm:px-4 md:px-8">
+        <MobileHeader />
+
+        <div className="mx-auto hidden w-full max-w-7xl px-8 md:block">
           <div className="flex h-16 items-center justify-between">
             <div className="flex md:px-0">
               <Link href="/">
@@ -53,25 +67,21 @@ const Header: FC<Props> = ({ hideSearch }) => {
               </Link>
               <ul className={`ml-10 flex space-x-4`}>
                 {mainNav.map(({ id, text, link }) => (
-                  <li key={id} className={`${buildStyles(link)}`}>
+                  <li key={id} className={`lg:text-lg ${buildStyles(link)}`}>
                     <ButtonLink href={link}>{text}</ButtonLink>
                   </li>
                 ))}
               </ul>
             </div>
             <div className="flex max-w-[65%] flex-1 items-center justify-center md:ml-6 md:max-w-none md:justify-end">
-              <div className="w-full max-w-lg md:max-w-sm">
-                {router.asPath !== "/" &&
-                  !router.asPath.startsWith("/pro") &&
-                  !router.asPath.startsWith("/blog") &&
-                  !hideSearch && <Searchbar />}
+              <div className="w-full max-w-sm lg:mr-4">
+                {router.asPath.startsWith("/search") && <Searchbar />}
               </div>
             </div>
             <ArrowButton href="/dashboard">Go to dashboard</ArrowButton>
           </div>
         </div>
       </nav>
-      <MobileHeader />
     </>
   );
 };
--- components/Header/MobileHeader.tsx
+++ components/Header/MobileHeader.tsx
@@ -1,7 +1,10 @@
+import { useRouter } from "next/router";
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { DialogOverlay, DialogContent } from "@reach/dialog";
+
+import Searchbar from "../algolia/Searchbar";
 
 import styles from "styles/MobileHeader.module.css";
 
@@ -13,12 +16,11 @@ const mainNav = [
 
 const MobileHeader = () => {
   const [showMenu, setshowMenu] = useState(false);
+  const router = useRouter();
 
   return (
-    <nav
-      className={`my-container sticky top-0 z-10 border-b border-gray-300 bg-white py-2 md:hidden`}
-    >
-      <header className="flex items-center justify-between">
+    <div className="my-container md:hidden">
+      <header className="flex items-center justify-between gap-3">
         <Link href="/">
           <a className="flex flex-shrink-0 items-center">
             <Image
@@ -29,6 +31,9 @@ const MobileHeader = () => {
             />
           </a>
         </Link>
+        <div className="w-[calc(100%-100px)]">
+          {router.asPath.startsWith("/search") && <Searchbar />}
+        </div>
         <button
           onClick={() => setshowMenu(!showMenu)}
           className={`${styles.burger} ${showMenu && styles.active}`}
@@ -36,19 +41,22 @@ const MobileHeader = () => {
       </header>
 
       <DialogOverlay isOpen={showMenu} onDismiss={() => setshowMenu(false)}>
-        <DialogContent className={styles.dialogContent}>
-          <ul className={`space-y-4`}>
+        <DialogContent
+          aria-label="Mobile nav links"
+          className={styles.dialogContent}
+        >
+          <ul className={`space-y-10`}>
             {mainNav.map(({ id, text, link }) => (
               <li key={id}>
                 <Link href={link}>
-                  <a>{text}</a>
+                  <a className="text-xl uppercase">{text}</a>
                 </Link>
               </li>
             ))}
           </ul>
         </DialogContent>
       </DialogOverlay>
-    </nav>
+    </div>
   );
 };
 
