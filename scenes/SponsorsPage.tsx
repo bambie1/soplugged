825ea7f77ca951b2e5517fd101d2cfa2980eac01
Benaiah Barango <bennieb96@gmail.com>
2022-09-13 23:33:39 -0400

    revert sponsors page

--- scenes/SponsorsPage.tsx
+++ scenes/SponsorsPage.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import dynamic from "next/dynamic";
+import { ArrowButton } from "@/styled/ArrowButton";
 
 const Header = dynamic(() => import("../components/Header/Header"));
 const Footer = dynamic(() => import("../components/Footer/Footer"));
@@ -7,50 +8,40 @@ const Footer = dynamic(() => import("../components/Footer/Footer"));
 const SponsorsPage = () => {
   return (
     <>
-      {/* <Header /> */}
-      <div className="relative flex h-screen snap-start">
-        <div className="my-container flex flex-1">
-          <div className="flex-1 flex-col justify-center pt-16 pb-10 lg:flex lg:max-w-[60%] lg:items-start">
-            <div className="my-4 flex bg-gray-400/20 lg:hidden">
-              <iframe
-                src="https://kweeve.page/soplugged/embed"
-                style={{ border: "none", flex: "1" }}
-                width="100%"
-                height="575px"
-                allow="payment"
-              ></iframe>
-            </div>
+      <Header />
+      <div className="my-container grid items-center gap-10 pt-16 pb-10 lg:grid-cols-5">
+        <div className="lg:col-span-3">
+          <h1 className="mb-6 text-4xl font-bold lg:text-6xl">Sponsors</h1>
+          <p className="w-[90%] lg:text-lg">
+            At SoPlugged, our biggest inspiration is supporting one another and
+            growing our community. Our goal is to normalize buying black and we
+            rely on amazing people like you to keep our platform free and
+            accessible to Black-owned businesses across Canada.
+          </p>
 
-            <h1 className="mb-6 text-4xl font-bold lg:text-6xl">Sponsors</h1>
-            <p className="w-[90%] lg:text-lg">
-              At SoPlugged, our biggest inspiration is supporting one another
-              and growing our community. Our goal is to normalize buying black
-              and we rely on amazing people like you to keep our platform free
-              and accessible to Black-owned businesses across Canada.
-            </p>
-
-            <p className="mt-3 w-[80%] italic">
-              All donations go towards maintaining our platform and supporting
-              Black-owned businesses across Canada.
-            </p>
-          </div>
+          <p className="mt-3 mb-10 w-[80%] italic">
+            All donations go towards maintaining our platform and supporting
+            Black-owned businesses across Canada.
+          </p>
+          <ArrowButton href="/our-story">
+            Read about the SoPlugged story
+          </ArrowButton>
         </div>
-        <aside className="absolute top-0 right-0 hidden h-full w-[40%] bg-gradient-to-t from-secondary to-white lg:flex">
-          <div className="relative flex flex-1 items-center justify-center overflow-hidden px-10 pt-24 pb-10">
-            <div className="h-[30rem] overflow-auto bg-gray-400/20">
-              <iframe
-                src="https://kweeve.page/soplugged/embed"
-                style={{ border: "none" }}
-                width="100%"
-                height="700px"
-                allow="payment"
-              ></iframe>
-            </div>
+
+        <aside className="col-span-2">
+          <div className="max-w-[400px] overflow-hidden rounded-lg border-2 border-primary shadow-2xl">
+            <iframe
+              src="https://kweeve.page/soplugged/embed"
+              style={{ border: "none" }}
+              width="100%"
+              height="550px"
+              allow="payment"
+            ></iframe>
           </div>
         </aside>
       </div>
 
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
