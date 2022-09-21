e2c33838b6d118f89f85f899190fdcd769928d8e
Benaiah Barango <bennieb96@gmail.com>
2022-09-20 23:23:58 -0400

    new pro hero design

--- components/pro/ProHero.tsx
+++ components/pro/ProHero.tsx
@@ -1,46 +1,37 @@
 import Link from "next/link";
-import { FC, useEffect, useState } from "react";
-
-const business_types = ["e-commerce", "freelance"];
-
-const BusinessTypes = () => {
-  const [textIndex, setTextIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTextIndex((prevIndex) =>
-        prevIndex === business_types.length - 1 ? 0 : prevIndex + 1
-      );
-    }, 7000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <span className="absolute left-1/2 -top-6 w-full -translate-x-1/2 font-handwriting text-2xl text-accent-dark transition duration-200 md:-left-10 md:-top-6 md:w-auto md:translate-x-0 lg:-top-8 xl:-left-16 xl:text-3xl">
-      {business_types[textIndex]}
-    </span>
-  );
-};
+import { FC } from "react";
 
 const ProHero: FC = () => {
   return (
     <>
       <div className="my-container relative flex items-center justify-center">
-        <div className="absolute inset-0 -z-[1] bg-radial-pro"></div>
-        <div className="mx-auto flex flex-col items-center py-10 text-center md:py-20">
-          <h1 className="mb-6 text-6xl font-bold lg:max-w-4xl lg:text-7xl xl:text-[6rem]">
-            Grow your{" "}
-            <span className="relative mt-6 inline-flex md:mt-0">
+        {/* <div className="absolute inset-0 -z-[1] bg-radial-pro"></div> */}
+        <div className="relative mx-auto flex flex-col items-center py-10 text-center md:py-20">
+          <h1 className="relative mx-auto mb-3 pb-2 text-7xl font-bold lg:mb-6 lg:max-w-5xl lg:text-8xl xl:text-[7rem]">
+            <span className="relative inline-flex">
               <img
-                src="/caret.svg"
+                src="doodles/pen-tool.svg"
                 alt=""
-                className="absolute -left-5 -bottom-4 hidden w-8 md:inline-block lg:-left-6 lg:bottom-0 lg:w-10"
+                className="absolute top-4 -left-10 h-12 grayscale-[.4] md:top-10"
               />
-              <BusinessTypes />
-              business
+              Design.
             </span>{" "}
-            <span className="relative inline-block">with ease</span>
+            <span className="relative inline-flex">
+              <img
+                src="doodles/zap.svg"
+                alt=""
+                className="absolute top-4 -right-10 h-12 grayscale-[.4] md:top-10"
+              />
+              Build.
+            </span>{" "}
+            <span className="relative inline-block bg-gradient-to-r from-accent-dark to-black bg-clip-text pb-2 text-transparent">
+              <img
+                src="doodles/send.svg"
+                alt=""
+                className="absolute top-4 -right-10 h-12 grayscale-[.4] md:top-10"
+              />{" "}
+              Launch.
+            </span>
           </h1>
           <p className="lg:max-w-3xl lg:text-xl xl:text-2xl">
             Hire our team of experts to handle your digital needs, from custom
