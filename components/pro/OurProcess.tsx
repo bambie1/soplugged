7137a455fc1d44d3a04f2d10811459bebb5bb877
Benaiah Barango <bennieb96@gmail.com>
2022-09-22 22:14:34 -0400

    add arrows to our process

--- components/pro/OurProcess.tsx
+++ components/pro/OurProcess.tsx
@@ -10,17 +10,31 @@ const OurProcess = () => {
       <div className="relative mx-auto flex max-w-3xl items-center justify-center gap-10 lg:gap-20">
         {steps.map(({ title, image }, index) => (
           <div
-            className="flex max-w-[5rem] flex-col items-center text-center"
+            className="relative flex max-w-[7rem] flex-col items-center text-center"
             key={title}
           >
+            {index === 0 && (
+              <img
+                alt=""
+                src="/doodles/up_right_arrow.svg"
+                className="absolute -right-10 -top-6 w-10 lg:-right-20 lg:w-20"
+              />
+            )}
+            {index === 1 && (
+              <img
+                alt=""
+                src="/doodles/down_right_arrow.svg"
+                className="absolute -right-10 -bottom-6 w-10 lg:-right-20 lg:w-20"
+              />
+            )}
             <div
               className={`flex aspect-square w-10 rounded-full ${
                 index % 2 === 0 ? "bg-gradient-to-r" : "bg-gradient-to-b"
-              } from-accent-dark to-black p-3 lg:w-24 lg:p-6`}
+              } mb-2 from-accent to-white p-3 lg:w-24 lg:p-7`}
             >
               <img src={image} alt="" />
             </div>
-            <p>{title}</p>
+            <p className="font-medium leading-tight">{title}</p>
           </div>
         ))}
       </div>
