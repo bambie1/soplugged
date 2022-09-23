8bab2c153ba641eb948a05c1cf9520e45b1a962d
Benaiah Barango <bennieb96@gmail.com>
2022-09-23 17:27:06 -0400

    add interval to hero text

--- components/pro/ProHero.tsx
+++ components/pro/ProHero.tsx
@@ -1,14 +1,35 @@
 import Link from "next/link";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+
+const features = [
+  { title: "Websites", class: "from-black to-accent-dark" },
+  { title: "Social content", class: "from-black to-secondary" },
+  { title: "Photography", class: "from-primary to-accent" },
+];
 
 const ProHero: FC = () => {
+  const [textIndex, setTextIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTextIndex((prevIndex) =>
+        prevIndex === features.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <div className="my-container relative flex flex-col items-center justify-center">
         <div className="relative mx-auto flex flex-col items-center pt-10 text-center md:pt-20">
-          <h1 className="relative mx-auto mb-3 pb-2 text-6xl font-bold lg:mb-6 lg:max-w-5xl lg:text-7xl xl:text-[5rem]">
-            <span className="bg-gradient-to-l from-black to-accent-dark bg-clip-text text-transparent">
-              Websites
+          <h1 className="relative mx-auto mb-3 pb-2 text-6xl font-bold transition duration-1000 lg:mb-6 lg:max-w-5xl lg:text-7xl xl:text-[5rem]">
+            <span
+              key={textIndex}
+              className={`roll-out inline-block bg-gradient-to-l ${features[textIndex].class} bg-clip-text pb-2 text-transparent`}
+            >
+              {features[textIndex].title}
             </span>{" "}
             your <br></br>customers will love
           </h1>
