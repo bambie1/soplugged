9cd4813f51027c8323003719821e9dbf70b78ade
Benaiah Barango <bennieb96@gmail.com>
2022-09-26 20:33:34 -0400

    tidy-up review page on my-business

--- components/BusinessForm/forms/6_Review.tsx
+++ components/BusinessForm/forms/6_Review.tsx
@@ -1,7 +1,12 @@
 import { useFormikContext } from "formik";
-
-import { Input } from "@/styled/Input";
-import TextArea from "@/styled/TextArea/TextArea";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import {
+  faGlobe,
+  faMapMarkerAlt,
+  faPhone,
+  faShapes,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Review = () => {
   const {
@@ -20,37 +25,80 @@ const Review = () => {
   } = useFormikContext<any>();
 
   return (
-    <>
-      <Input noHelper value={business_name} label="Business Name" disabled />
-      <Input
-        noHelper
-        value={business_location}
-        label="Business Location"
-        disabled
-      />
+    <div className="grid gap-5 text-gray-700">
+      <div className="mb-3 flex items-center gap-3">
+        {logo_url && (
+          <img
+            src={logo_url}
+            alt="business logo"
+            className="h-10 w-10 rounded-full"
+          />
+        )}
+        <p className="text-xl font-light uppercase text-primary lg:text-3xl">
+          {business_name}
+        </p>
+      </div>
+
+      <div className="flex flex-wrap gap-4">
+        {category && (
+          <div>
+            <FontAwesomeIcon icon={faShapes} className="mr-2" />
+            {category}
+          </div>
+        )}
+
+        {business_location && (
+          <div>
+            <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" />
+            {business_location}
+          </div>
+        )}
+      </div>
 
       {sample_images && (
-        <p>{sample_images.split(",").length} image(s) uploaded</p>
+        <div className="grid max-w-sm grid-cols-3 items-center gap-3">
+          {sample_images.split(",").map((image: string) => (
+            <img
+              src={image}
+              key={image}
+              alt=""
+              className="max-h-36 w-full rounded-lg object-cover"
+            />
+          ))}
+        </div>
       )}
 
-      <Input noHelper value={category} label="Category" disabled />
-      {ig_handle && (
-        <Input noHelper value={ig_handle} label="IG Handle" disabled />
-      )}
-      {phone_number && (
-        <Input noHelper value={phone_number} label="Phone Number" disabled />
-      )}
-      {business_url && (
-        <Input noHelper value={business_url} label="Business Url" disabled />
-      )}
+      <div className="mt-4 gap-3">
+        <span className="font-medium uppercase text-gray-700 underline">
+          Description:
+        </span>
+        <div
+          dangerouslySetInnerHTML={{ __html: business_description }}
+          className="text-ellipsis text-sm text-gray-500 lg:text-base"
+        ></div>
+      </div>
 
-      <TextArea
-        value={business_description?.replace(/<[^>]*>?/gm, "") || ""}
-        label="Description"
-        noScroll
-        disabled
-      />
-    </>
+      <div className="flex flex-wrap gap-5">
+        {ig_handle && (
+          <p className="flex items-center gap-1 text-sm lg:text-base">
+            <FontAwesomeIcon className="text-gray-500" icon={faInstagram} />
+            {ig_handle}
+          </p>
+        )}
+        {phone_number && (
+          <p className="flex items-center gap-1 text-sm lg:text-base">
+            <FontAwesomeIcon className="text-gray-500" icon={faPhone} />
+            {phone_number}
+          </p>
+        )}
+        {business_url && (
+          <p className="flex items-center gap-1 text-sm lg:text-base">
+            <FontAwesomeIcon className="text-gray-500" icon={faGlobe} />
+            {business_url}
+          </p>
+        )}
+      </div>
+    </div>
   );
 };
 
