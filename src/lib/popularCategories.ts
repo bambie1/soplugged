9ddacf62c8a1fd1bb2e7f6b8ec30d3788a2b2e04
Benaiah Barango <bennieb96@gmail.com>
2022-10-03 19:54:27 -0400

    fix hero section categories

--- src/lib/popularCategories.ts
+++ src/lib/popularCategories.ts
@@ -5,11 +5,11 @@ export const popularCategories = [
   },
   {
     url: "https://images.unsplash.com/photo-1608039783021-6116a558f0c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWVhdCUyMHBpZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
-    title: "Food / Catering",
+    title: "Baking / Catering",
   },
   {
     url: "https://images.unsplash.com/photo-1567531708788-4c44105d00ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmxhY2slMjBwaG90b2dyYXBoZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
-    title: "Photography",
+    title: "Media services",
   },
   {
     url: "https://images.unsplash.com/photo-1502005229762-cf1b2da7c5d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aW50ZXJpb3IlMjBkZXNpZ258ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60",
