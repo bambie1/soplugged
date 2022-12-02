502743b8ede9a40e602a3fdee25c0ec08b2ec9c0
Benaiah Barango <benaiah.barango@fullscript.com>
2022-12-02 15:51:15 -0500

    update introduction page for existing businesses

--- components/BusinessForm/0_Introduction.tsx
+++ components/BusinessForm/0_Introduction.tsx
@@ -1,7 +1,11 @@
 import { FC } from "react";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
-import { GlobeAltIcon, ScaleIcon } from "@heroicons/react/outline";
+import {
+  ArrowRightIcon,
+  GlobeAltIcon,
+  ScaleIcon,
+} from "@heroicons/react/outline";
 
 import { BusinessForm } from "layouts/BusinessForm";
 import { useBusinessStore } from "@/scenes/MyBusinessPage/MyBusinessPage";
@@ -41,7 +45,10 @@ const features = [
 ];
 
 const Introduction: FC = () => {
-  const { handleNextStep, steps } = useBusinessStore();
+  const { handleNextStep, steps, business, updateCurrentStep } =
+    useBusinessStore();
+
+  const isNewBusiness = !business?.slug;
 
   const { handleSubmit } = useForm();
 
@@ -49,39 +56,74 @@ const Introduction: FC = () => {
     handleNextStep();
   };
 
+  const renderIntro = () => (
+    <div className="grid grid-cols-1 gap-y-10 md:grid-cols-2 md:gap-x-12 md:gap-y-16">
+      {features.map((feature) => (
+        <div
+          key={feature.name}
+          className="relative flex flex-col gap-2 sm:flex-row md:flex-col lg:flex-row lg:gap-6"
+        >
+          <div className="flex h-8 w-8 items-center justify-center rounded-xl border border-primary text-primary sm:shrink-0 lg:h-12 lg:w-12">
+            <feature.icon
+              className="aspect-square h-5 lg:h-8"
+              aria-hidden="true"
+              strokeWidth={0.7}
+            />
+          </div>
+          <div className="sm:min-w-0 sm:flex-1">
+            <p className="text-lg font-semibold text-gray-900">
+              {feature.name}
+            </p>
+            <p className="mt-2 leading-7 text-gray-600 lg:text-base">
+              {feature.description}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+
+  const renderSteps = () => {
+    return (
+      <div className="grid grid-cols-1 gap-y-7 md:grid-cols-2 md:gap-y-10 md:gap-x-10">
+        {steps.map((step) => {
+          const isDisabled = step.number === steps.length;
+          return (
+            <button
+              key={step.number}
+              onClick={() => updateCurrentStep(step.number)}
+              className="group flex flex-col rounded-lg border border-transparent p-2 text-left transition duration-150 hover:shadow-sm disabled:hover:shadow-none md:p-3"
+              disabled={isDisabled}
+            >
+              <p className="flex items-center gap-3 text-lg font-semibold text-gray-700">
+                {step.name}
+                <ArrowRightIcon
+                  className={`h-6 w-6 transition duration-150 ${
+                    !isDisabled && "group-hover:translate-x-2"
+                  }`}
+                  strokeWidth={0.5}
+                />
+              </p>
+              <p className="text-gray-600 lg:text-base">{step.description}</p>
+            </button>
+          );
+        })}
+      </div>
+    );
+  };
+
   return (
     <form className="grid gap-4" onSubmit={handleSubmit(onSubmit)}>
       <BusinessForm
-        title="Welcome aboard!"
-        subtitle="We're happy to have your business listed on our directory"
+        title={isNewBusiness ? "Welcome aboard!" : "Welcome back!"}
+        subtitle={
+          isNewBusiness
+            ? "We're happy to have your business listed on our directory"
+            : "Pick any step from below to edit, or just 'Begin editing'"
+        }
         isWide
       >
-        <div className="">
-          <div className="grid grid-cols-1 gap-y-10 md:grid-cols-2 md:gap-x-12 md:gap-y-16">
-            {features.map((feature) => (
-              <div
-                key={feature.name}
-                className="relative flex flex-col gap-2 sm:flex-row md:flex-col lg:flex-row lg:gap-6"
-              >
-                <div className="flex h-8 w-8 items-center justify-center rounded-xl border border-primary text-primary sm:shrink-0 lg:h-12 lg:w-12">
-                  <feature.icon
-                    className="aspect-square h-5 lg:h-8"
-                    aria-hidden="true"
-                    strokeWidth={0.7}
-                  />
-                </div>
-                <div className="sm:min-w-0 sm:flex-1">
-                  <p className="text-lg font-semibold text-gray-900">
-                    {feature.name}
-                  </p>
-                  <p className="mt-2 leading-7 text-gray-600 lg:text-base">
-                    {feature.description}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        {isNewBusiness ? renderIntro() : renderSteps()}
       </BusinessForm>
     </form>
   );
--- scenes/MyBusinessPage/MyBusinessPage.tsx
+++ scenes/MyBusinessPage/MyBusinessPage.tsx
@@ -1,7 +1,5 @@
 import { useRouter } from "next/router";
 import { FC, useEffect, useRef, useState } from "react";
-import toast from "react-hot-toast";
-import { useSWRConfig } from "swr";
 import create from "zustand";
 
 import NameLocation from "@/components/BusinessForm/1_NameLocation";
@@ -38,6 +36,7 @@ interface FormState {
   handleNextStep: () => void;
   handlePreviousStep: () => void;
   steps: typeof BusinessSteps;
+  updateCurrentStep: (value: number) => void;
 }
 
 export const useBusinessStore = create<FormState>()((set) => ({
@@ -57,12 +56,11 @@ export const useBusinessStore = create<FormState>()((set) => ({
   handlePreviousStep: () => {
     return set(({ currentStep }) => ({ currentStep: currentStep - 1 }));
   },
+  updateCurrentStep: (value) => set(() => ({ currentStep: value })),
 }));
 
 const MyBusinessPage: FC<Props> = ({ business }) => {
-  const { mutate } = useSWRConfig();
   const router = useRouter();
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const [showExitModal, setShowExitModal] = useState(false);
   const { currentStep, steps, updateBusiness, handlePreviousStep } =
     useBusinessStore();
@@ -100,44 +98,6 @@ const MyBusinessPage: FC<Props> = ({ business }) => {
       default:
         return <Review />;
     }
-  }
-
-  async function _submitForm(values: any, actions: any) {
-    setIsSubmitting(true);
-
-    const businessObj = {
-      ...values,
-      business_url:
-        values.business_url && values.business_url !== "undefined"
-          ? "https://" + values.business_url
-          : "",
-    };
-
-    const updatedBusiness = await fetch("/api/user/updateBusiness", {
-      method: !business ? "POST" : "PATCH",
-      body: JSON.stringify({
-        data: businessObj,
-        isNew: !business,
-      }),
-    }).then((res) => res.json());
-
-    if (updatedBusiness.statusCode !== 500) {
-      toast.success(
-        !business
-          ? "Business created successfully"
-          : "Business updated successfully"
-      );
-      mutate(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/business`);
-      mutate(
-        `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/business?slug=${updatedBusiness.slug}`
-      );
-      router.push(`/business/${updatedBusiness.slug}`);
-    } else {
-      toast.error("An error occurred");
-      setIsSubmitting(false);
-    }
-
-    actions.setSubmitting(false);
   }
 
   return (
