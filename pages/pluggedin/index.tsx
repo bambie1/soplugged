4d1e92699f4a9654a3d70e5ec2bfd1f9054c6191
Benaiah Barango <benaiah.barango@fullscript.com>
2022-12-10 13:19:10 -0500

    add guests section

--- pages/pluggedin/index.tsx
+++ pages/pluggedin/index.tsx
@@ -1,5 +1,30 @@
-import { PageWrapper } from "@/components/PageWrapper";
+import { Footer } from "@/components/Footer";
+import Header from "@/components/Header/Header";
 import { SEO } from "@/components/SEO";
+
+const otherGuests = [
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1618085222100-93f0eecad0aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGJsYWNrJTIwd29tYW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    name: "Nina Barango",
+    title: "Host",
+    description: "Creator and founder, SoPlugged",
+  },
+  {
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1663050697267-35243ea0817d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGRqfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    name: "Leon Suave",
+    title: "DJ",
+    description: "Award-winning DJ",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1581368129682-e2d66324045b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aG9zdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+    name: "Fikayo A.",
+    title: "MC",
+    description: "Canadian TV Personality",
+  },
+];
 
 const PluggedInHomePage = () => {
   return (
@@ -9,8 +34,9 @@ const PluggedInHomePage = () => {
         description="Network with fellow business-owners, share ideas and learn how to manage your finances as a business owner"
       />
 
-      <PageWrapper>
-        <div className="my-container relative text-center">
+      <>
+        <Header />
+        <div className="my-container relative pt-28 text-center">
           <div className="absolute inset-0 -z-10 bg-radial-pluggedin"></div>
           <p>SoPlugged's 2nd anniversary!</p>
           <h1 className="mb-4 text-5xl font-extrabold sm:text-6xl lg:text-7xl">
@@ -47,7 +73,64 @@ const PluggedInHomePage = () => {
             </svg>
           </button>
         </div>
-      </PageWrapper>
+
+        <div className="mt-10">
+          <div className="flex justify-center gap-2">
+            <p>FEBRUARY 18, 2023</p>
+            <p>6PM</p>
+          </div>
+
+          <div className="light-gradient relative mt-20 py-10 pt-28 lg:py-20">
+            <div className="absolute -top-20 left-0 w-full">
+              <div className="mx-auto mt-3 grid max-w-md grid-cols-4 items-center px-2">
+                <div className="flex aspect-[3/4] flex-col items-center justify-center gap-1 rounded-lg border border-primary bg-white p-2">
+                  <p className="text-3xl font-bold lg:text-5xl">15</p>
+                  <p className="text-sm font-light lg:text-base">Days</p>
+                </div>
+                <div className="ml-auto flex aspect-[3/4] w-[85%] flex-col items-center justify-center rounded-lg border border-primary bg-white p-2">
+                  <p className="text-2xl lg:text-3xl">20</p>
+                  <p className="text-xs font-light lg:text-sm">Hours</p>
+                </div>
+                <div className="ml-auto flex aspect-[3/4] w-[85%] flex-col items-center justify-center rounded-lg border border-primary bg-white p-2">
+                  <p className="text-2xl lg:text-3xl">40</p>
+                  <p className="text-xs font-light lg:text-sm">Minutes</p>
+                </div>
+                <div className="ml-auto flex aspect-[3/4] w-[85%] flex-col items-center justify-center rounded-lg border border-primary bg-white p-2">
+                  <p className="text-2xl lg:text-3xl">10</p>
+                  <p className="text-xs font-light lg:text-sm">Seconds</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="my-container grid items-center sm:grid-cols-2 lg:grid-cols-4">
+              <div>
+                <img
+                  src="reni.jpg"
+                  alt="Portrait of Reni Odetoyinbo"
+                  className="transparentShadow mb-3 aspect-[3/4] w-full rounded-lg border border-primary object-cover lg:mb-4"
+                />
+                <p>KEYNOTE SPEAKER</p>
+                <p>Reni Odetoyinbo</p>
+                <p>Marketing consultant and finance enthusiast</p>
+              </div>
+              {otherGuests.map(({ name, imageUrl, title, description }) => (
+                <div key={name} className="mx-auto w-[80%] lg:mr-0 lg:ml-auto">
+                  <img
+                    src={imageUrl}
+                    alt={`Portrait of ${name}`}
+                    className="aspect-[3/4] w-full rounded-lg border border-primary object-cover lg:mb-4"
+                  />
+                  <p>{title}</p>
+                  <p>{name}</p>
+                  <p className="lg:text-base">{description}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <Footer />
+      </>
     </>
   );
 };
