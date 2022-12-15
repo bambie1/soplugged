c9897182250581a194a69e58b6e02eb4c9df5329
Benaiah Barango <benaiah.barango@fullscript.com>
2022-12-15 17:39:41 -0500

    push to prevew domain

--- components/pluggedIn/Tickets.tsx
+++ components/pluggedIn/Tickets.tsx
@@ -1,3 +1,11 @@
+import { CheckIcon } from "@heroicons/react/outline";
+
+const features = [
+  "5 products",
+  "Up to 1,000 subscribers",
+  "Basic analytics",
+  "48-hour support response time",
+];
 const options = [
   {
     title: "Early bird",
@@ -25,24 +33,33 @@ const Tickets = () => {
       <div className="mt-6 grid lg:grid-cols-2">
         <div>
           <p>With a ticket purchase, you get access to:</p>
-          <ul>
-            <li>Raffle draw entry (Only early-birds)</li>
-            <li>All events scheduled for the evening</li>
-            <li>Drinks and snacks</li>
-            <li>Keynote speech</li>
+          <ul role="list" className="mt-6 space-y-6 divide-y divide-gray-200">
+            {features.map((feature) => (
+              <li key={feature} className="flex">
+                <CheckIcon
+                  className="h-6 w-6 flex-shrink-0 text-indigo-500"
+                  aria-hidden="true"
+                />
+                <span className="ml-3 text-gray-500">{feature}</span>
+              </li>
+            ))}
           </ul>
         </div>
         <ul className="grid gap-4">
           {options.map((option) => (
             <li key={option.title}>
               <button
-                className={`flex flex-col gap-4 rounded-3xl p-6 text-left sm:p-10 lg:flex-row lg:items-center ${
+                disabled={!option.isAvailable}
+                className={`flex flex-col gap-4 rounded-3xl p-6 text-left sm:p-10 ${
                   option.isAvailable
-                    ? "light-gradient priceButton border border-primary"
+                    ? "priceButton border border-primary bg-white"
                     : "bg-gray-50"
                 }`}
               >
                 <div className="lg:min-w-0 lg:flex-1">
+                  {!option.isAvailable && (
+                    <span className="text-gray-600">Not available</span>
+                  )}
                   <h3 className="text-lg font-bold leading-8 tracking-tight text-primary">
                     {option.title} ticket
                   </h3>
@@ -50,15 +67,17 @@ const Tickets = () => {
                     {option.description}
                   </div>
                 </div>
-                <div>
-                  <a
-                    href="#"
-                    className="inline-block rounded-lg bg-primary/5 px-4 py-2.5 text-center text-sm font-bold leading-5 text-primary hover:bg-primary/10"
-                  >
-                    Buy {option.title} ticket{" "}
-                    <span aria-hidden="true">&rarr;</span>
-                  </a>
-                </div>
+                {option.isAvailable && (
+                  <div>
+                    <a
+                      href="#"
+                      className="inline-block rounded-lg bg-primary/5 px-4 py-2.5 text-center text-sm font-bold leading-5 text-primary hover:bg-primary/10"
+                    >
+                      Buy {option.title} ticket{" "}
+                      <span aria-hidden="true">&rarr;</span>
+                    </a>
+                  </div>
+                )}
               </button>
             </li>
           ))}
