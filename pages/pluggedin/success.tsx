fc787910b09a71dc2ea26c746dedb0431f7fdfbf
Benaiah Barango <benaiah.barango@fullscript.com>
2022-12-19 08:38:19 -0500

    use tweet intent instead

--- pages/pluggedin/success.tsx
+++ pages/pluggedin/success.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import { TwitterShareButton } from "react-share";
 
 import { Footer } from "@/components/Footer";
 import Header from "@/components/Header/Header";
@@ -49,29 +48,28 @@ const PluggedInSuccessPage = (
             to expect at the event.
           </p>
 
-          <TwitterShareButton
-            url={shareUrl}
-            // title="Just got my ticket for #PluggedIn by SoPlugged, claim yours!"
-            className="mt-20"
+          <a
+            href={`https://twitter.com/intent/tweet?text=Just got my ticket for #PluggedIn by SoPlugged, claim yours!&url=${shareUrl}`}
+            className="neuButton mt-20"
+            target="_blank"
+            rel="noreferrer"
           >
-            <div className="neuButton">
-              Share on Twitter
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="1"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="feather feather-twitter"
-              >
-                <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
-              </svg>
-            </div>
-          </TwitterShareButton>
+            Share on Twitter
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-twitter"
+            >
+              <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+            </svg>
+          </a>
         </div>
       </div>
 
