1f159ef14cd04fae2f46172900cdb63a773e34fc
Benaiah Barango <benaiah.barango@fullscript.com>
2023-02-16 17:31:27 -0500

    new design

--- src/components/algolia/SoPluggedProAd.tsx
+++ src/components/algolia/SoPluggedProAd.tsx
@@ -1,3 +1,5 @@
+import { ExternalLinkIcon } from "@heroicons/react/outline";
+import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
 
@@ -8,8 +10,8 @@ interface Props {
 const SoPluggedProAd: FC<Props> = ({ adType }) => {
   const imageSource =
     adType === "website"
-      ? "/illustrations/pro-boost-1.png"
-      : "/illustrations/pro-boost-2.png";
+      ? "/illustrations/shopify.svg"
+      : "/illustrations/instagram.svg";
 
   const adText =
     adType === "website"
@@ -18,32 +20,34 @@ const SoPluggedProAd: FC<Props> = ({ adType }) => {
 
   return (
     <Link href="/pro">
-      <a className="group my-4 flex flex-col">
-        <div className="relative flex aspect-video w-full flex-col overflow-hidden rounded-lg border border-primary/50 p-4 pt-6 shadow transition duration-200 group-hover:scale-[.98] lg:pt-8">
-          <img
-            src={imageSource}
-            alt=""
-            className="absolute top-0 right-0 bottom-0 -z-[1] h-full"
-          />
-          <p className="mb-2 w-[90%] font-bold sm:text-lg xl:text-xl">
-            Try <span className="underline">SoPlugged PRO</span> today
+      <a target="_blank" className="group my-4 flex flex-col">
+        <div className="relative flex aspect-video w-full flex-col overflow-hidden rounded-lg bg-gradient-to-b from-tr-pink-100 to-indigo-100 p-4 shadow-md transition duration-200 group-hover:scale-[.98] md:aspect-video lg:p-5">
+          <div className="mb-2 flex items-center justify-between">
+            <div className="relative aspect-video h-8">
+              <Image src={imageSource} alt="" className="" layout="fill" />
+            </div>
+            <ExternalLinkIcon className="h-6 w-6" />
+          </div>
+          <p className="mb-2 text-xl font-bold">
+            Try <span className="text-primary underline">SoPlugged PRO</span>{" "}
+            today
           </p>
-          <p className="mb-2 w-[90%] text-sm text-gray-700 xl:w-[70%]">
-            {adText}
-          </p>
-
-          <Link href="/pro">
-            <a className="mt-auto text-sm underline">Learn more</a>
-          </Link>
+          <p className="mb-2 text-sm text-gray-700 lg:w-[90%]">{adText}</p>
+          <div className="mt-auto flex items-center justify-between">
+            <Link href="/pro">
+              <a className="text-sm underline">Learn more</a>
+            </Link>
+            <div>
+              <span className="text-sm font-semibold text-primary">
+                SoPlugged
+              </span>
+              <span className="ml-1 rounded-md bg-primary px-1 py-[.125rem] text-xs font-medium text-white">
+                TEAM
+              </span>
+            </div>
+          </div>
         </div>
         <div className="mt-3 flex items-center justify-between">
-          <div>
-            <span className="font-semibold text-primary">SoPlugged</span>
-            <span className="ml-1 rounded-md bg-primary px-1 py-[.125rem] text-xs font-medium text-white lg:text-sm">
-              TEAM
-            </span>
-          </div>
-
           <p className="font-bold text-gray-500 lg:text-sm">PROMOTED</p>
         </div>
       </a>
