bbf9661f80074376e607a578f8c52386cce4dfa1
Benaiah Barango <bennieb96@gmail.com>
2023-02-17 10:06:23 -0500

    Display SoPlugged pro CTA in search results (#48)
    * display soplugged pro ad in search page
    
    * add pro ad to search page
    
    * new design
    
    * fix styles
    
    * margin
    
    * add tracking ref
    
    * use onclick tracking

---
+++ src/components/algolia/SoPluggedProAd.tsx
@@ -0,0 +1,77 @@
+import { FC } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  ExternalLinkIcon,
+  InformationCircleIcon,
+} from "@heroicons/react/outline";
+import { usePlausible } from "next-plausible";
+
+interface Props {
+  adType: "website" | "social-media";
+}
+
+const SoPluggedProAd: FC<Props> = ({ adType }) => {
+  const plausible = usePlausible();
+
+  const imageSource =
+    adType === "website"
+      ? "/illustrations/shopify.svg"
+      : "/illustrations/instagram.svg";
+
+  const adText =
+    adType === "website"
+      ? "Hire our website experts to launch and grow an online presence for your business"
+      : "Hire our social-media experts to launch and grow your business's online presence";
+
+  const adClickHandler = () => {
+    plausible("Click pro ad");
+    window.open("/pro", "_blank");
+  };
+
+  return (
+    <li className="my-4">
+      <a
+        role="button"
+        onClick={adClickHandler}
+        key="main-link"
+        className="group relative flex w-full flex-col overflow-hidden rounded-lg bg-gradient-to-b from-tr-pink-100 to-indigo-100 p-4 shadow-md transition duration-200 hover:scale-[.98]"
+      >
+        <div className="mb-1 flex items-center justify-between">
+          <div className="relative aspect-video h-10">
+            <Image src={imageSource} alt="" layout="fill" />
+          </div>
+          <ExternalLinkIcon className="h-6 w-6 transition duration-200 group-hover:text-gray-500" />
+        </div>
+        <p className="mb-2 text-xl font-bold">
+          Try <span className="text-primary underline">SoPlugged PRO</span>{" "}
+          today
+        </p>
+        <p className="mb-4 text-sm text-gray-700 lg:w-[90%]">{adText}</p>
+        <div className="mt-auto flex items-center justify-between">
+          <span className="text-sm underline transition duration-200 group-hover:no-underline">
+            Learn more
+          </span>
+          <div>
+            <span className="text-sm font-semibold text-primary">
+              SoPlugged
+            </span>
+            <span className="ml-1 rounded-md bg-primary px-1 py-[.125rem] text-xs font-medium text-white">
+              TEAM
+            </span>
+          </div>
+        </div>
+      </a>
+      <div className="mt-2 flex items-center justify-between">
+        <p className="font-bold text-gray-500 lg:text-base">PROMOTED</p>
+        <Link href="/pro?ref=explore_page">
+          <a key="info" aria-label="Learn more">
+            <InformationCircleIcon className="h-5 w-5 text-gray-500" />
+          </a>
+        </Link>
+      </div>
+    </li>
+  );
+};
+
+export default SoPluggedProAd;
