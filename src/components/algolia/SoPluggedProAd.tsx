4d49cba273544c319abc517aeaa65e0c0a3b973e
Benaiah Barango <benaiah.barango@fullscript.com>
2023-02-17 09:15:20 -0500

    use onclick tracking

--- src/components/algolia/SoPluggedProAd.tsx
+++ src/components/algolia/SoPluggedProAd.tsx
@@ -5,12 +5,15 @@ import {
   ExternalLinkIcon,
   InformationCircleIcon,
 } from "@heroicons/react/outline";
+import { usePlausible } from "next-plausible";
 
 interface Props {
   adType: "website" | "social-media";
 }
 
 const SoPluggedProAd: FC<Props> = ({ adType }) => {
+  const plausible = usePlausible();
+
   const imageSource =
     adType === "website"
       ? "/illustrations/shopify.svg"
@@ -21,40 +24,44 @@ const SoPluggedProAd: FC<Props> = ({ adType }) => {
       ? "Hire our website experts to launch and grow an online presence for your business"
       : "Hire our social-media experts to launch and grow your business's online presence";
 
+  const adClickHandler = () => {
+    plausible("Click pro ad");
+    window.open("/pro", "_blank");
+  };
+
   return (
     <li className="my-4">
-      <Link href="/pro?ref=explore_page">
-        <a
-          target="_blank"
-          key="main-link"
-          className="group relative flex w-full flex-col overflow-hidden rounded-lg bg-gradient-to-b from-tr-pink-100 to-indigo-100 p-4 shadow-md transition duration-200 hover:scale-[.98]"
-        >
-          <div className="mb-1 flex items-center justify-between">
-            <div className="relative aspect-video h-10">
-              <Image src={imageSource} alt="" layout="fill" />
-            </div>
-            <ExternalLinkIcon className="h-6 w-6 transition duration-200 group-hover:text-gray-500" />
+      <a
+        role="button"
+        onClick={adClickHandler}
+        key="main-link"
+        className="group relative flex w-full flex-col overflow-hidden rounded-lg bg-gradient-to-b from-tr-pink-100 to-indigo-100 p-4 shadow-md transition duration-200 hover:scale-[.98]"
+      >
+        <div className="mb-1 flex items-center justify-between">
+          <div className="relative aspect-video h-10">
+            <Image src={imageSource} alt="" layout="fill" />
           </div>
-          <p className="mb-2 text-xl font-bold">
-            Try <span className="text-primary underline">SoPlugged PRO</span>{" "}
-            today
-          </p>
-          <p className="mb-4 text-sm text-gray-700 lg:w-[90%]">{adText}</p>
-          <div className="mt-auto flex items-center justify-between">
-            <span className="text-sm underline transition duration-200 group-hover:no-underline">
-              Learn more
+          <ExternalLinkIcon className="h-6 w-6 transition duration-200 group-hover:text-gray-500" />
+        </div>
+        <p className="mb-2 text-xl font-bold">
+          Try <span className="text-primary underline">SoPlugged PRO</span>{" "}
+          today
+        </p>
+        <p className="mb-4 text-sm text-gray-700 lg:w-[90%]">{adText}</p>
+        <div className="mt-auto flex items-center justify-between">
+          <span className="text-sm underline transition duration-200 group-hover:no-underline">
+            Learn more
+          </span>
+          <div>
+            <span className="text-sm font-semibold text-primary">
+              SoPlugged
             </span>
-            <div>
-              <span className="text-sm font-semibold text-primary">
-                SoPlugged
-              </span>
-              <span className="ml-1 rounded-md bg-primary px-1 py-[.125rem] text-xs font-medium text-white">
-                TEAM
-              </span>
-            </div>
+            <span className="ml-1 rounded-md bg-primary px-1 py-[.125rem] text-xs font-medium text-white">
+              TEAM
+            </span>
           </div>
-        </a>
-      </Link>
+        </div>
+      </a>
       <div className="mt-2 flex items-center justify-between">
         <p className="font-bold text-gray-500 lg:text-base">PROMOTED</p>
         <Link href="/pro?ref=explore_page">
