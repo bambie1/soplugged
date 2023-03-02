3e875b97781fbc8412669e11e85a4ee8d7e5abda
Benaiah Barango <benaiah.barango@fullscript.com>
2023-03-02 14:48:23 -0500

    remove border on last element

--- src/components/blog/BlogList.tsx
+++ src/components/blog/BlogList.tsx
@@ -12,7 +12,10 @@ const BlogList: FC<Props> = ({ posts }) => {
     <ul className="inline-flex w-full flex-wrap gap-8 md:grid md:grid-cols-2 xl:gap-12 xl:gap-y-20">
       {posts?.map((post) => {
         return (
-          <li key={post.slug} className="w-full border-b-2 md:border-none">
+          <li
+            key={post.slug}
+            className="w-full border-b-2 last:border-none md:border-none"
+          >
             <BlogCard post={post} />
           </li>
         );
