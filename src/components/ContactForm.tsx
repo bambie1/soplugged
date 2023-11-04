0de685668a0093992b8f4d6060dc7a5c7c400c98
Benaiah Barango <bennieb96@gmail.com>
2023-11-04 11:24:08 -0400

    tsc

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,11 +1,11 @@
 import { FC } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import toast from "react-hot-toast";
-import { signIn, useSession } from "next-auth/react";
 
 import { Button } from "@/styled/Button";
 
 interface IFormInput {
+  email: string;
   message: string;
 }
 
@@ -16,17 +16,9 @@ interface Props {
 }
 
 const ContactForm: FC<Props> = ({ businessEmail }) => {
-  const { data: session } = useSession();
-
   const { register, handleSubmit, reset } = useForm<IFormInput>();
 
-  const disabled = !session?.user || session?.user.email === businessEmail;
-
   const onSubmit: SubmitHandler<IFormInput> = async (data) => {
-    if (!session?.user?.email) return;
-
-    const { email: userEmail } = session.user;
-
     const email = {
       From: "hello@soplugged.com",
       To: businessEmail,
@@ -35,7 +27,7 @@ const ContactForm: FC<Props> = ({ businessEmail }) => {
       TemplateModel: {
         product_name: "SoPlugged Business",
         body: data.message,
-        user_email: userEmail,
+        user_email: data.email,
       },
     };
 
@@ -50,41 +42,6 @@ const ContactForm: FC<Props> = ({ businessEmail }) => {
     } else toast.error("An error occurred");
   };
 
-  const renderButton = () => {
-    if (session?.user?.email)
-      return (
-        <div className="grid">
-          <Button type="submit" disabled={disabled}>
-            Send Message
-          </Button>
-          <p className="mt-1 text-center text-xs text-gray-500 lg:text-sm">
-            Signed in as{" "}
-            <span className="font-medium underline">{session.user.email}</span>
-          </p>
-        </div>
-      );
-
-    return <Button onClick={() => signIn()}>Sign in to send message</Button>;
-  };
-
-  const renderContent = () => {
-    return (
-      <div className="grid">
-        <label htmlFor="message" className="mb-1 block text-sm font-medium">
-          Message
-        </label>
-        <textarea
-          placeholder="Begin your message..."
-          id="message"
-          className="rounded-lg shadow-sm placeholder:text-gray-300 focus:border-primary focus:ring-transparent"
-          rows={10}
-          disabled={disabled}
-          {...register("message", { required: true })}
-        />
-      </div>
-    );
-  };
-
   return (
     <form
       className="grid gap-4 pt-4 text-left"
@@ -94,8 +51,19 @@ const ContactForm: FC<Props> = ({ businessEmail }) => {
         Contact owner
       </h3>
 
-      {renderContent()}
-      {renderButton()}
+      <div className="grid">
+        <label htmlFor="message" className="mb-1 block text-sm font-medium">
+          Message
+        </label>
+        <textarea
+          placeholder="Begin your message..."
+          id="message"
+          className="rounded-lg shadow-sm placeholder:text-gray-300 focus:border-primary focus:ring-transparent"
+          rows={10}
+          {...register("message", { required: true })}
+        />
+      </div>
+      <Button type="submit">Send Message</Button>
     </form>
   );
 };
