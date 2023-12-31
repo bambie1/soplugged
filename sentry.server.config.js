75f1a898d715defc79cf00504acfc4dc06982a01
Benaiah Barango <bennieb96@gmail.com>
2023-12-31 16:41:08 -0500

    fix sentry

--- sentry.server.config.js
+++
@@ -1,7 +0,0 @@
-import * as Sentry from "@sentry/nextjs";
-
-Sentry.init({
-  dsn: "https://c7a983b60e504ec1a993994b4dcff264@o509845.ingest.sentry.io/5604856",
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0,
-});
