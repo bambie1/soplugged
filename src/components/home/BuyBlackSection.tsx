c9c8729b7d587a6cc31961ed4936d957d3bc8c71
Benaiah Barango <benaiah.a.barango@gmail.com>
2024-11-13 06:27:10 -0500

    simplify

--- src/components/home/BuyBlackSection.tsx
+++ src/components/home/BuyBlackSection.tsx
@@ -1,6 +1,27 @@
+import clsx from "clsx";
+
 export const BuyBlackSection = ({ content }: { content: any }) => {
-  // console.log(content.businesses);
   const businesses = content.businesses;
+
+  const renderCard = (business: any, isWide?: boolean) => {
+    return (
+      <div
+        className={clsx(
+          "relative overflow-hidden rounded-xl opacity-40 hover:opacity-100",
+          {
+            "aspect-[2/3]": !isWide,
+            "aspect-[3/2]": isWide,
+          },
+        )}
+      >
+        <img
+          src={business.sample_images[0].asset.url}
+          alt=""
+          className="absolute inset-0 h-full w-full object-cover object-top"
+        />
+      </div>
+    );
+  };
 
   return (
     <div className="page-section lg:pt-36">
@@ -10,84 +31,24 @@ export const BuyBlackSection = ({ content }: { content: any }) => {
 
       <div className="mt-10 grid grid-cols-2 items-start gap-4 px-4 sm:px-6 xl:grid-cols-6 xl:gap-6 xl:px-12">
         <div className="grid grid-rows-2 gap-6">
-          <div className="relative aspect-[2/3] overflow-hidden rounded-xl opacity-40 hover:opacity-100">
-            <img
-              src={businesses[0].sample_images[0].asset.url}
-              alt=""
-              className="absolute inset-0 h-full w-full object-cover object-top"
-            />
-          </div>
-          <div className="relative aspect-[2/3] overflow-hidden rounded-xl opacity-40 hover:opacity-100">
-            <img
-              src={businesses[1].sample_images[0].asset.url}
-              alt=""
-              className="absolute inset-0 h-full w-full object-cover object-top"
-            />
-          </div>
+          {renderCard(businesses[0])}
+          {renderCard(businesses[1])}
         </div>
         <div className="mt-20 grid grid-rows-2 gap-6">
-          <div className="relative aspect-[2/3] overflow-hidden rounded-xl opacity-40 hover:opacity-100">
-            <img
-              src={businesses[2].sample_images[0].asset.url}
-              alt=""
-              className="absolute inset-0 h-full w-full object-cover object-top"
-            />
-          </div>
-          <div className="relative aspect-[2/3] overflow-hidden rounded-xl opacity-40 hover:opacity-100">
-            <img
-              src={businesses[3].sample_images[0].asset.url}
-              alt=""
-              className="absolute inset-0 h-full w-full object-cover object-top"
-            />
-          </div>
+          {renderCard(businesses[2])}
+          {renderCard(businesses[3])}
         </div>
         <div className="mt-40 grid grid-rows-2 gap-6 xl:col-span-2">
-          <div className="relative aspect-[3/2] overflow-hidden rounded-xl opacity-40 hover:opacity-100">
-            <img
-              src={businesses[4].sample_images[0].asset.url}
-              alt=""
-              className="absolute inset-0 h-full w-full object-cover object-top"
-            />
-          </div>
-          <div className="relative aspect-[3/2] overflow-hidden rounded-xl opacity-40 hover:opacity-100">
-            <img
-              src={businesses[5].sample_images[0].asset.url}
-              alt=""
-              className="absolute inset-0 h-full w-full object-cover object-top"
-            />
-          </div>
+          {renderCard(businesses[4], true)}
+          {renderCard(businesses[5], true)}
         </div>
         <div className="mt-20 grid grid-rows-2 gap-6">
-          <div className="relative aspect-[2/3] overflow-hidden rounded-xl opacity-40 hover:opacity-100">
-            <img
-              src={businesses[6].sample_images[0].asset.url}
-              alt=""
-              className="absolute inset-0 h-full w-full object-cover object-top"
-            />
-          </div>
-          <div className="relative aspect-[2/3] overflow-hidden rounded-xl opacity-40 hover:opacity-100">
-            <img
-              src={businesses[7].sample_images[0].asset.url}
-              alt=""
-              className="absolute inset-0 h-full w-full object-cover object-top"
-            />
-          </div>
+          {renderCard(businesses[6])}
+          {renderCard(businesses[7])}
         </div>
         <div className="grid grid-rows-2 gap-6">
-          <div className="relative aspect-[2/3] overflow-hidden rounded-xl opacity-40 hover:opacity-100">
-            <img
-              src={businesses[8].sample_images[0].asset.url}
-              alt=""
-              className="absolute inset-0 h-full w-full object-cover object-top"
-            />
-          </div>
-          <div className="relative aspect-[2/3] overflow-hidden rounded-xl opacity-40 hover:opacity-100">
-            <img
-              src={businesses[9].sample_images[0].asset.url}
-              alt=""
-              className="absolute inset-0 h-full w-full object-cover object-top"
-            />
-          </div>
+          {renderCard(businesses[8])}
+          {renderCard(businesses[9])}
         </div>
       </div>
     </div>
