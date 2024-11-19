8f7e7d41c2ed7bd2ccb609b4e3421888d47a257e
Benaiah Barango <benaiah.a.barango@gmail.com>
2024-11-18 23:12:25 -0500

    supabase results

--- src/app/(rest)/directory/page.tsx
+++ src/app/(rest)/directory/page.tsx
@@ -1,11 +1,20 @@
+import { createClient } from "@/utils/supabase/server";
+
 import { SearchBar } from "./search-bar";
 import { SearchResults } from "./search-results";
 
 export default async function DirectoryPage() {
+  const supabase = await createClient();
+  const { data: businesses } = await supabase
+    .from("businesses")
+    .select()
+    .order("confidence_rating", { ascending: true })
+    .order("sample_images", { ascending: true });
+
   return (
     <>
       <SearchBar />
-      <SearchResults />
+      <SearchResults businesses={businesses ?? []} />
     </>
   );
 }
--- src/app/(rest)/directory/search-results.tsx
+++ src/app/(rest)/directory/search-results.tsx
@@ -1,11 +1,27 @@
-export const SearchResults = () => {
+export const SearchResults = ({ businesses }: { businesses?: any[] }) => {
   return (
     <div className="mt-10 grid gap-6 px-4 sm:grid-cols-2 lg:mt-20 lg:grid-cols-3 lg:gap-8">
-      {[1, 2, 3, 4, 5, 6].map((i) => (
-        <div key={i} className="">
-          <div className="aspect-video border"></div>
-        </div>
-      ))}
+      {businesses?.map((business) => {
+        const mainImage = business.sample_images?.split(",")[0];
+
+        return (
+          <div key={business} className="">
+            <div className="aspect-video border">
+              {mainImage && (
+                <img
+                  src={mainImage}
+                  alt=""
+                  className="h-full w-full object-cover"
+                />
+              )}
+            </div>
+
+            <p className="text-primary">
+              {business.business_name} ({business.confidence_rating})
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 };
