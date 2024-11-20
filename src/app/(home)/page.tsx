fd3b534e8d07a2e5c6bdcd3b1672891c3a0b7cf8
Benaiah Barango <benaiah.a.barango@gmail.com>
2024-11-20 16:16:33 -0500

    pillars

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -12,6 +12,7 @@ import { SubscribeBanner } from "@/components/shared/SubscribeBanner";
 import { client } from "@/sanity/lib/client";
 import { urlFor } from "@/sanity/lib/image";
 
+import { Pillars } from "./pillars";
 import { HOME_PAGE_QUERY, HOME_POSTS_QUERY } from "./queries";
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -39,35 +40,7 @@ export default async function Home() {
       <main className="bg-black text-white selection:bg-light/90 selection:text-black">
         {content.video && <VideoHero content={content} />}
 
-        <div className="mx-auto mt-10 grid max-w-screen-2xl gap-8 px-4 lg:grid-cols-3">
-          <div className="flex aspect-[3/1] flex-col border-t border-white/20 py-4">
-            <div className="mb-4 h-12 w-12 rounded-full border bg-tbm-gradient"></div>
-            <p className="mb-4 text-xl font-medium">Amplify</p>
-            <p className="opacity-70">
-              Showcase your work, share your story, and let your business or
-              creative vision shine across our platform. Together, we amplify
-              the voices shaping our future.
-            </p>
-          </div>
-          <div className="flex aspect-[3/1] flex-col border-t border-white/20 py-4">
-            <div className="mb-4 h-12 w-12 rounded-full border"></div>
-            <p className="mb-4 text-xl font-medium">Connect</p>
-            <p className="opacity-70">
-              Build meaningful relationships with like-minded creators,
-              entrepreneurs, and leaders. Find collaboration opportunities and
-              grow your network in a thriving community.
-            </p>
-          </div>
-          <div className="flex aspect-[3/1] flex-col border-t border-white/20 py-4">
-            <div className="mb-4 h-12 w-12 rounded-full border"></div>
-            <p className="mb-4 text-xl font-medium">Educate</p>
-            <p className="opacity-70">
-              Access valuable resources, attend workshops, and gain the
-              knowledge you need to take your business or creative pursuits to
-              the next level.
-            </p>
-          </div>
-        </div>
+        <Pillars />
 
         <div className="page-section padded mt-10">
           <div className="flex max-w-2xl flex-col items-start">
---
+++ src/app/(home)/pillars.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import clsx from "clsx";
+import Link from "next/link";
+import { useState } from "react";
+
+const pillars = [
+  {
+    title: "Amplify",
+    description:
+      "Showcase your work, share your story, and let your business or creative vision shine across our platform. Together, we amplify the voices shaping our future.",
+    href: "#amplify",
+  },
+  {
+    title: "Connect",
+    description:
+      "Build meaningful relationships with like-minded creators, entrepreneurs, and leaders. Find collaboration opportunities and grow your network in a thriving community.",
+    href: "#amplify",
+  },
+  {
+    title: "Educate",
+    description:
+      "Access valuable resources, attend workshops, and gain the knowledge you need to take your business or creative pursuits to the next level.",
+    href: "#amplify",
+  },
+];
+
+export const Pillars = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  return (
+    <div className="mx-auto mt-10 grid max-w-screen-2xl gap-8 px-4 lg:grid-cols-3">
+      {pillars.map((pillar, index) => (
+        <Link
+          href={pillar.href}
+          key={pillar.title}
+          className={clsx(
+            "group flex aspect-[3/1] flex-col border-t border-white py-4 transition-all duration-150",
+            {
+              "opacity-100": activeIndex === index,
+              "opacity-50": activeIndex !== index,
+            },
+          )}
+          onMouseOver={() => setActiveIndex(index)}
+        >
+          <div className="mb-4 h-12 w-12 rounded-full border"></div>
+          <p className="mb-4 text-xl font-medium">{pillar.title}</p>
+          <p className="opacity-70">{pillar.description}</p>
+        </Link>
+      ))}
+    </div>
+  );
+};
