954394fc0210ba940111448f463b9e9d281af64e
Benaiah Barango <benaiah.a.barango@gmail.com>
2024-11-23 05:13:54 -0500

    fix layout

--- src/app/(rest)/directory/layout.tsx
+++ src/app/(rest)/directory/layout.tsx
@@ -29,28 +29,3 @@ export default function DirectoryLayout({
     </div>
   );
 }
-
-export const DirectoryHero = () => {
-  return (
-    <div className="relative z-10">
-      <Header />
-
-      <div className="absolute left-0 top-0 -z-10 h-80 w-full bg-gradient-to-b from-[#F2EDE3] to-white"></div>
-
-      <div className="mx-auto flex max-w-3xl flex-col items-center pb-16 pt-28 text-center text-primary lg:pt-48">
-        <h1 className="mb-4">Find Black-owned businesses near you</h1>
-        <p className="lg:text-lg">
-          From hair salons to event planning teams across Canada, our directory
-          helps you #buyblack
-        </p>
-      </div>
-
-      <div className="mb-20">
-        <SearchBar />
-        <SearchResults />
-      </div>
-
-      <SubscribeBanner />
-    </div>
-  );
-};
