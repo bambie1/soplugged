eb7e4271df38006bd9923d870fa190ebc8ea213a
Benaiah Barango <benaiah.a.barango@gmail.com>
2024-11-27 16:11:16 -0500

    separate component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -10,6 +10,7 @@ import supabase from "@/utils/supabase/server";
 
 import { ConnectPillar } from "./connect-pillar";
 import { GrowthPillar } from "./growth-pillar";
+import { MissionStatement } from "./mission-statement";
 import { Pillars } from "./pillars";
 import { PodcastPillar } from "./podcast-pillar";
 import { HOME_PAGE_QUERY, HOME_POSTS_QUERY } from "./queries";
@@ -49,32 +50,7 @@ export default async function Home() {
 
         <Pillars />
 
-        <div className="page-section relative mt-10">
-          <img
-            alt="lines"
-            decoding="async"
-            data-nimg="1"
-            className="absolute bottom-40 left-1/2 top-0 z-0 h-full w-[2148px] min-w-[2148px] -translate-x-[calc(50%-110px)] opacity-10 md:bottom-0 lg:opacity-20"
-            src="/waves.svg"
-          ></img>
-
-          <div className="padded flex flex-col items-start bg-black">
-            <p className="z-10 mb-2 font-medium text-yellow-50 opacity-70">
-              Our journey began with a simple question
-            </p>
-            <p className="z-10 mb-8 max-w-2xl text-3xl tracking-tight lg:text-4xl xl:text-5xl">
-              How can we close the gap for Black entrepreneurs who often face
-              unique challenges in visibility and resources?
-            </p>
-
-            <Link
-              href="/our-story"
-              className="z-10 py-8 underline decoration-yellow-100 underline-offset-8 transition-all duration-150 hover:underline-offset-4"
-            >
-              Read our story
-            </Link>
-          </div>
-        </div>
+        <MissionStatement />
 
         {content.podcastHighlight && (
           <PodcastPillar content={content.podcastHighlight} />
