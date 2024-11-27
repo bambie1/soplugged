e9bde78eb9a2ec93b957f54fb4a83e08cc112e1f
Benaiah Barango <benaiah.a.barango@gmail.com>
2024-11-27 08:23:01 -0500

    react player

--- src/app/(home)/connect-pillar.tsx
+++ src/app/(home)/connect-pillar.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import { useState } from "react";
+import ReactPlayer from "react-player";
 
 import { PillarHeading } from "./pillar-heading";
 
 export const ConnectPillar = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-
   return (
     <>
       <div className="page-section" id="connect">
@@ -19,33 +15,26 @@ export const ConnectPillar = () => {
           link={{ href: "/events", text: "See upcoming events" }}
         />
 
-        <div className="padded">
-          <div className="relative mt-10 aspect-video overflow-hidden rounded-xl border border-white/20">
-            <img
-              src="/events_filler.jpeg"
-              alt=""
-              className="h-full w-full object-cover"
+        <div className="lg:padded relative">
+          <div className="relative mt-10 aspect-video w-full overflow-hidden border-white/20 lg:rounded-2xl lg:border">
+            <ReactPlayer
+              url="https://stream.mux.com/e7a4SX301G4CLNgh71V47gY8ewUULZucTZsQpsIUpOHQ.m3u8"
+              width="100%"
+              height="100%"
+              muted
+              loop
+              playing
             />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <div className="aspect-square w-12 rounded-full bg-white opacity-75 lg:w-24"></div>
-            </div>
+          </div>
 
-            <div className="absolute bottom-4 right-4 flex flex-col items-end lg:bottom-10 lg:right-10">
-              <p className="text-sm font-semibold lg:text-lg">
-                Black Market at Stackt
-              </p>
-              <p className="text-xs opacity-80 lg:text-base">October 2024</p>
-            </div>
+          <div className="absolute bottom-4 right-4 flex flex-col items-end lg:bottom-10 lg:right-10">
+            <p className="text-sm font-semibold lg:text-lg">
+              Black Market at Stackt
+            </p>
+            <p className="text-xs opacity-80 lg:text-base">October 2024</p>
           </div>
         </div>
       </div>
-
-      {/* <iframe
-          src="https://drive.google.com/file/d/1OhEe1hCfdfgV_TY8w5lipOmlnLN1964N/preview"
-          width="640"
-          height="480"
-          allow="autoplay"
-        ></iframe> */}
     </>
   );
 };
