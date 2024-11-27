6372b1b38e1211d6fa2d9106d261bc1c478a7ff9
Benaiah Barango <benaiah.a.barango@gmail.com>
2024-11-27 13:03:26 -0500

    svg lines

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -43,21 +43,29 @@ export default async function Home() {
 
         <Pillars />
 
-        <div className="page-section padded mt-10">
-          <div className="flex max-w-2xl flex-col items-start">
-            <p className="mb-2 font-medium text-yellow-50 opacity-70">
+        <div className="page-section relative mt-10">
+          <img
+            alt="lines"
+            decoding="async"
+            data-nimg="1"
+            className="animate__animated animate__fadeIn absolute bottom-40 left-1/2 top-0 z-0 h-full w-[2148px] min-w-[2148px] -translate-x-[calc(50%-110px)] grayscale md:bottom-0 xl:object-cover"
+            src="https://restream.io/_next/static/media/about-hero-lines.1dea24a7a4e438b684e2f1fe10dc9b43.svg"
+          ></img>
+
+          <div className="padded flex flex-col items-start bg-black">
+            <p className="z-10 mb-2 font-medium text-yellow-50 opacity-70">
               Our journey began with a simple question
             </p>
-            <p className="mb-8 text-3xl tracking-tight lg:text-4xl xl:text-5xl">
+            <p className="z-10 mb-8 max-w-2xl text-3xl tracking-tight lg:text-4xl xl:text-5xl">
               How can we close the gap for Black entrepreneurs who often face
               unique challenges in visibility and resources?
             </p>
 
             <Link
               href="/our-story"
-              className="py-8 underline decoration-yellow-100 underline-offset-8 transition-all duration-150 hover:underline-offset-4"
+              className="z-10 py-8 underline decoration-yellow-100 underline-offset-8 transition-all duration-150 hover:underline-offset-4"
             >
-              Learn more about us
+              Read our story
             </Link>
           </div>
         </div>
