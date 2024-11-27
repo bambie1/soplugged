0f14b4de9a2ef6f2da284f84b7bb6634c995da7e
Benaiah Barango <benaiah.a.barango@gmail.com>
2024-11-27 11:19:19 -0500

    use pillar heading

--- src/app/(home)/pillar-heading.tsx
+++ src/app/(home)/pillar-heading.tsx
@@ -1,21 +1,27 @@
+import clsx from "clsx";
 import Link from "next/link";
 
 export const PillarHeading = ({
   title,
   subTitle,
   link,
+  isLight,
 }: {
   title: string;
   subTitle: string;
   link: { href: string; text: string };
+  isLight?: boolean;
 }) => {
   return (
     <div className="padded">
-      <div className="flex flex-wrap items-center justify-between gap-6 lg:gap-10">
+      <div
+        className={clsx(
+          "flex flex-wrap items-center justify-between gap-6 lg:gap-10",
+          isLight ? "text-slate-800" : "text-yellow-50",
+        )}
+      >
         <div className="flex max-w-2xl flex-col items-start">
-          <p className="mb-2 font-medium text-yellow-50 opacity-70">
-            {subTitle}
-          </p>
+          <p className="mb-2 font-medium opacity-70">{subTitle}</p>
           <p className="max-w-2xl text-4xl tracking-tight lg:mb-8 lg:text-4xl xl:text-5xl">
             {title}
           </p>
@@ -23,7 +29,10 @@ export const PillarHeading = ({
 
         <Link
           href={link.href}
-          className="underline decoration-yellow-100 underline-offset-8 transition-all duration-150 hover:underline-offset-4 lg:py-8"
+          className={clsx(
+            "underline underline-offset-8 transition-all duration-150 hover:underline-offset-4 lg:py-8",
+            isLight ? "decoration-slate-400" : "decoration-yellow-100",
+          )}
         >
           {link.text}
         </Link>
--- src/app/(home)/recent-blogs.tsx
+++ src/app/(home)/recent-blogs.tsx
@@ -1,34 +1,21 @@
-import Link from "next/link";
-
 import BlogCard from "@/components/shared/BlogCard";
+
+import { PillarHeading } from "./pillar-heading";
 
 export const RecentBlogs = ({ posts }: { posts: any[] }) => {
   return (
     <div className="page-section bg-white text-black" id="connect">
-      <div className="padded">
-        <div className="flex items-center justify-between gap-10">
-          <div className="flex max-w-2xl flex-col items-start">
-            <p className="mb-2 font-medium text-slate-800 opacity-70">
-              Latest Blog Posts
-            </p>
-            <p className="mb-8 max-w-2xl text-2xl tracking-tight lg:text-4xl xl:text-5xl">
-              Helpful resources to grow your business as an entrepreneur{" "}
-            </p>
-          </div>
+      <PillarHeading
+        subTitle="Recent Blog Posts"
+        title="Helpful resources to grow your business as an entrepreneur"
+        link={{ href: "/blog", text: "See all posts" }}
+        isLight
+      />
 
-          <Link
-            href="/blog"
-            className="py-8 underline decoration-slate-400 underline-offset-8 transition-all duration-150 hover:underline-offset-4"
-          >
-            See all posts
-          </Link>
-        </div>
-
-        <div className="mt-10 grid gap-10 lg:grid-cols-3">
-          {posts.map((post: any) => (
-            <BlogCard post={post} key={post.title} />
-          ))}
-        </div>
+      <div className="padded mt-10 grid gap-10 lg:grid-cols-3">
+        {posts.map((post: any) => (
+          <BlogCard post={post} key={post.title} />
+        ))}
       </div>
     </div>
   );
